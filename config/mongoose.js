66eb3069a7977cc1fe7b8cd702cff97fa0beb1fc
Ujawal Tiwari <ujawal0011@gmail.com>
2022-12-21 13:19:20 +0530

    database location

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/Hospital-API');
+mongoose.connect('mongodb://127.0.0.1:27017/Todo-Application');
 
 const db = mongoose.connection;
 
